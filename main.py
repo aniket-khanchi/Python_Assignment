d1147a3b3cd63eae8f1dd26ff594569afecb8a18
Aniket Khanchi <aniket.khanchi@iu-study.org>
2023-04-18 00:10:22 +0530

    added getting started files

---
+++ main.py
@@ -0,0 +1,17 @@
+# importing necessary modules
+import pandas as pd
+import math
+
+df_test = pd.read_csv("data/ideal.csv")
+df_train = pd.read_csv("data/train.csv")
+df_ideal_fun = pd.read_csv("data/ideal.csv")
+
+# print(df_test.head())
+
+class parse_csv:
+    def __init__(self) -> None:
+        """
+        
+        """
+        pass
+
